05db9f21b550a8f8a9e3e184c2c06e4f83199afc
mikus <mikolaj.rychel@gmail.com>
2019-03-08 19:30:19 +0100

    Siemano kolano

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import cv2 as cv
 from export_data import readImage
 import numpy
 
-
-
-
 # Image read
 img = cv.imread("photos/test.jpg", cv.IMREAD_GRAYSCALE)
 img = cv.medianBlur(img, 5)
 
 result, temp, contours, answers = readImage(img)
 
-answers.sort()
+answers.sort(key=lambda x: x[0][1])
 
 for item in answers:
     print(item)
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
+def normalize(temp: list, span: int):
+    master_index = 0
+    compared_index = 0
+
+
+
+    print(temp)
+
+
 temp = [[1604, 872], [1682, 876], [1681, 970], [1602, 968]]
-print("before : " + str(temp))
-temp.sort()
-print("ater : " + str(temp))
+temp.sort(key=lambda x: x[1])
+
+normalize(temp, 5)
+
+for item in temp:
+    item[1] = temp[0][1]
